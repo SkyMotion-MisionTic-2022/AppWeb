78726e629bb9ff19073adc508d4afc702af5f5d9
JuanVaquiro <jvaquiro23@itfip.edu.co>
2021-12-12 13:42:09 -0500

    stylos paginas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,14 @@ const Navbar = () => {
   return (
     <nav className="bg-blue-400">
       <ul className="flex w-full justify-between my-3">
-        <li><i className="fab fa-app-store p-1"></i></li>
+        <li>
+          <Link to="/auth/bienvenidos">
+            <i className="fab fa-app-store p-1"></i>
+          </Link>
+        </li>
         <li className="px-3 ">
           <button className="bg-blue-100 rounded-full border-2 border-blue-100 hover:border-blue-300  p-2">
-            <Link to='/auth/login'> Iniciar Sesión</Link>
+            <Link to="/auth/login"> Iniciar Sesión</Link>
           </button>
         </li>
       </ul>
